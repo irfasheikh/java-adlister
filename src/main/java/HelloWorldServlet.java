871807accad3910ecbabad4cd823117bb4955a54
Irfa Sheikh <irfasheikh@gmail.com>
2021-05-26 23:29:25 -0500

    servlets

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,19 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+//annotation goes here to annotate the whole thing
+@WebServlet(name = "HelloWorld", urlPatterns = "/hello")  // metadata to go along with our servlet
+public class HelloWorldServlet extends HttpServlet { //defining the class name / extending HttpServlet
+
+
+    //put doGet doPost methods in here
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().println("<h1> Hello World! It is I, Iffy!<h1>");
+
 
     }
 }
+
---
+++ src/main/java/pageCounterServlet.java
@@ -0,0 +1,18 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Page Counter", urlPatterns = "/count")
+
+public class pageCounterServlet extends HttpServlet {
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().println();
+
+
+    }
+}
+
